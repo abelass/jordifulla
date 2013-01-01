773d69ca7676f40b0f1296baf2605dc07a70c6da
Rainer <abelass@Chuperlapup.(none)>
2013-01-01 20:16:47 +0100

    maj

---
+++ old/js/oculta.js
@@ -0,0 +1,22 @@
+function tanca(id){
+if (document.getElementById){ //se obtiene el id
+var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
+el.style.display = (el.style.display == 'none') ? 'none' : 'none'; //damos un atributo display:none que oculta el div
+}
+}
+window.onload = function(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
+tanca('contacte');/* "contenido_a_mostrar" es el nombre que le dimos al DIV */
+}
+
+
+
+function obre(id){
+if (document.getElementById){ //se obtiene el id
+var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
+el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+}
+}
+window.onload = function(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
+obre('contacte');/* "contenido_a_mostrar" es el nombre que le dimos al DIV */
+}
+
